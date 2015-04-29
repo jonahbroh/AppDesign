00cfa16b1d469be740f1409827d887fa57fdb550
jonahbroh <jonahbroh@gmail.com>
2015-04-29 01:00:40 -0600

    Done.

--- Download/wget.js
+++ Download/wget.js
@@ -5,73 +5,47 @@ try{
   var lines = fs.readFileSync(process.argv[2]).toString().split("\n");
 }
 catch(e){
-    console.log("Nope.");
+    console.log("Format correctly, please.");
     process.exit(1);
 }
 
 var download = function(url, dest, cb) {
-  var file;
-  try{
-      file = fs.createWriteStream(dest);
-      console.log(url +" - "+ dest);
-  }
-  catch(e){
-      console.log("Actual location, please.");
-      fs.unlink(dest);
-      return;
-  }
-  var request;
-  try{
-     request = http.get( url, function( response ) {
-        console.log( "get callback!" );
-        response.pipe( file );
+  var file = fs.createWriteStream(dest);
+  console.log(url +" will be saved to "+ dest+".");
+  var request = http.get( url, function( response ) {
+  response.pipe( file );
         file.on( 'finish', function() {
-            console.log( "finish callback!" );
             // close() is async, call cb after close completes.
             file.close( cb );
         });
     });
     request.on( 'error', function( err ) { // Handle errors
-        console.log( "error callback!" );
-        // Delete the file async. (But we don't check the result)
-        fs.unlink(dest);
-        if( cb )
-            cb( err.message );
+        fs.unlink(dest, cb);
+        console.log("Unfortunately, the file at "+url+" could not be saved to "+dest+".");
     });
-    }
-  catch(e){
-    console.log("File, please.");
-    return;
-  }
+
 };
 
-/*function readFile( f )
-{
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function ()
-    {
-        if(xmlhttp.readyState == 4){
-            prepare( xmlhttp.responseText );
-        }
-    }
-    xmlhttp.open( "GET", f, true );
-    xmlhttp.send();
-    console.log( "Sent" );
-}*/
 
-function prepare( )
+if(lines.length%2 !== 0)
 {
-    //var lines = param.split("\n");
-    /*if(lines.length%2 != 0)
-    {
-        console.log("Even number of lines, please");
-    }*/
-    function callb() { console.log( "main cb" ) };
-    for(var i = 0; i < lines.length; i = i + 2)
-    {
-        download(lines[i], lines[i+1], callb );
-    }
+    console.log("Even number of lines, please.");
 }
-
-prepare();
-console.log("done");
+function callb() { }
+for(var i = 0; i < lines.length; i = i + 2)
+{
+  //Makes sure the file destination is valid.
+  var possible;
+  try {
+    var fd = fs.openSync(lines[i+1], 'w');
+    fs.closeSync(fs.openSync(lines[i+1], 'w'));
+    possible = true;
+  }
+  catch (e) {
+    possible = false;
+    console.log("A file cannot be saved to " +lines[i+1]+".");
+  }
+  if(possible){
+    download(lines[i], lines[i+1], callb );
+  }
+}
