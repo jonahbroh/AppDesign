7d1441a76c956edd9eee1089aa8ce725da41d7f7
jonahbroh <jonahbroh@gmail.com>
2015-04-29 00:07:57 -0600

    still no downloads?

--- Download/wget.js
+++ Download/wget.js
@@ -10,16 +10,39 @@ catch(e){
 }
 
 var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
+  var file;
+  try{
+      file = fs.createWriteStream(dest);
+      console.log(url +" - "+ dest);
+  }
+  catch(e){
+      console.log("Actual location, please.");
+      fs.unlink(dest);
+      return;
+  }
+  var request;
+  try{
+     request = http.get( url, function( response ) {
+        console.log( "get callback!" );
+        response.pipe( file );
+        file.on( 'finish', function() {
+            console.log( "finish callback!" );
+            // close() is async, call cb after close completes.
+            file.close( cb );
+        });
     });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
-  });
+    request.on( 'error', function( err ) { // Handle errors
+        console.log( "error callback!" );
+        // Delete the file async. (But we don't check the result)
+        fs.unlink(dest);
+        if( cb )
+            cb( err.message );
+    });
+    }
+  catch(e){
+    console.log("File, please.");
+    return;
+  }
 };
 
 /*function readFile( f )
@@ -43,10 +66,12 @@ function prepare( )
     {
         console.log("Even number of lines, please");
     }*/
+    function callb() { console.log( "main cb" ) };
     for(var i = 0; i < lines.length; i = i + 2)
     {
-        download(lines[i], lines[i+1], function() { console.log( "main cb" ) } );
+        download(lines[i], lines[i+1], callb );
     }
 }
 
 prepare();
+console.log("done");
